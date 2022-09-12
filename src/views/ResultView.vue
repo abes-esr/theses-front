e465cba4d112fa54402004d1c47beaf1b02764ed
Théo Chambon <chambon@abes.fr>
2022-09-12 11:51:58 +0200

    test

--- src/views/ResultView.vue
+++ src/views/ResultView.vue
@@ -1,6 +1,6 @@
 <template>
     <div class="pa-8">
-        <h1> {{ JSON.parse(this.result).length }} résultats de thèses pour : {{ request }}</h1>
+        <h1> {{ JSON.parse(result).length }} résultats de thèses pour : {{ request }}</h1>
         <v-row>
             <v-col v-for="item in JSON.parse(result)" :key="item._source" cols="12" xs="12" md="4">
                 <result-card :titre="item._source.titre" :description="item._source.abstractFR"
