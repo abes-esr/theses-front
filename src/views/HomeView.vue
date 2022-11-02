5260f8171bf2b3d2106ba6a1f2c9d6fb8a58fe1d
Fabien Rétif <fabien.retif@zoho.com>
2022-11-02 15:18:03 +0100

    Ajout de la fonctionnalité de recherche de personnes + ajout d'une message box

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,23 +1,24 @@
 <template>
   <v-container class="justify-center">
+    <Message-box ref="messageBox"></Message-box>
     <v-row class="justify-center">
       <h1 class="pb-16 text-center">{{ $t("slogan") }}</h1>
     </v-row>
-    <domain-selector></domain-selector>
+    <domain-selector @select="modifierDomaine"></domain-selector>
     <v-row class="justify-center ma-0 pa-0">
       <v-col cols="12" md="9" xl="6">
-        <search-bar />
+        <Search-bar @onError="displayError"/>
       </v-col>
     </v-row>
     <v-row class="justify-center ma-0 pa-0">
       <v-col cols="12" md="3" xl="2">
-        <stats-card titre="546 000" :description="$t('referencés')" date="au 03/06/2022"></stats-card>
+        <Stats-card titre="546 000" :description="$t('referencés')" date="au 03/06/2022"></Stats-card>
       </v-col>
       <v-col cols="12" md="3" xl="2">
-        <stats-card titre="79 000" :description="$t('preparation')" date="au 03/06/2022"></stats-card>
+        <Stats-card titre="79 000" :description="$t('preparation')" date="au 03/06/2022"></Stats-card>
       </v-col>
       <v-col cols="12" md="3" xl="2">
-        <stats-card titre="805 000" :description="$t('personnesRef')" date="au 03/06/2022"></stats-card>
+        <Stats-card titre="805 000" :description="$t('personnesRef')" date="au 03/06/2022"></Stats-card>
       </v-col>
     </v-row>
     <v-row>
@@ -27,9 +28,22 @@
 </template>
 
 <script setup>
-import searchBar from '../components/search/SearchBar.vue'
-import statsCard from '../components/home/StatsCard.vue'
+import SearchBar from '../components/search/SearchBar.vue'
+import StatsCard from '../components/home/StatsCard.vue'
 import DomainSelector from '../components/search/DomainSelector.vue'
+import {thesesAPIService} from "@/services/ThesesAPI";
+import {ref} from "vue";
+import MessageBox from "@/components/common/MessageBox.vue";
+
+const {modifierDomaine} = thesesAPIService();
+
+const messageBox = ref(null);
+
+function displayError(message) {
+  messageBox.value?.open(message, {
+    type: "error"
+  })
+}
 
 </script>
 
