410ef0bc7f782a58a40bc82b4c5834400fd162d4
Fabien Rétif <fabien.retif@zoho.com>
2022-11-29 16:21:55 +0100

    Ajout de l'auto-complétion sur la barre de recherche Personnes

--- src/components/personnes/search/SearchBar.vue
+++ src/components/personnes/search/SearchBar.vue
@@ -1,7 +1,11 @@
 <template>
   <v-col>
-    <v-text-field clearable :label='$t("rechercher")' v-model="request" type="text" variant="outlined"
-                  @keydown.enter="search">
+    <v-combobox clearable @click:clear="clearSearch" :label='$t("rechercher")' v-model="request"
+                v-model:search="requestSearch" @update:modelValue="selectSuggestion"
+                variant="outlined" :items="items" item-title="suggestion"
+                item-value="suggestion"
+                :loading="isLoading" :menu="suggestionActive" cache-items hide-no-data
+                hide-selected no-filter @keydown.enter="search">
       <template v-slot:append>
         <v-btn color="primary"
                style="height: 100%; border-bottom-left-radius: 0; border-top-left-radius: 0; margin-left: -10px !important;"
@@ -9,18 +13,19 @@
           <v-icon large>mdi-magnify</v-icon>
         </v-btn>
       </template>
-    </v-text-field>
+    </v-combobox>
   </v-col>
 </template>
 <script>
 export default {
-name: "search-bar-personnes"
+  name: "search-bar-personnes"
 };
 </script>
 <script setup>
-import { ref, onMounted} from 'vue'
+import {ref, onMounted, watch} from 'vue'
 import {useRoute, useRouter} from 'vue-router'
-import { computed } from 'vue'
+import {computed} from 'vue'
+import {personnesAPIService} from "@/services/PersonnesAPI";
 
 const router = useRouter();
 const currentRoute = useRoute();
@@ -33,18 +38,23 @@ defineProps({
   },
 })
 const request = ref('');
-const emit = defineEmits(['search','onError']);
+const requestSearch = ref("");
+const emit = defineEmits(['search', 'onError']);
 
 onMounted(
-  () => {
-    if (currentRoute.query && currentRoute.query.q) {
-      request.value = currentRoute.query.q;
+    () => {
+      if (currentRoute.query && currentRoute.query.q) {
+        request.value = currentRoute.query.q;
+      }
     }
-  }
 )
 
+/**
+ * Fonction pour rechercher
+ * @returns {Promise<void>}
+ */
 async function search() {
-  let currentURLParams = Object.assign({},currentRoute.query);
+  let currentURLParams = Object.assign({}, currentRoute.query);
 
   if (currentURLParams) {
     currentURLParams.q = request.value
@@ -63,6 +73,60 @@ async function search() {
     })
   }
   emit('search', request.value);
+}
+
+/**
+ * Fonction lorsqu'on vide le champs de saisie
+ */
+function clearSearch() {
+  request.value = "";
+  search();
+}
+
+/* ---------------- */
+/* Auto-complétion  */
+/* ---------------- */
+
+const {suggestionPersonne} = personnesAPIService();
+const items = ref([]);
+const isLoading = ref(false);
+const suggestionActive = ref(false);
+
+watch(requestSearch, (candidate) => {
+  if (candidate != null && candidate.length > 2) {
+    getSuggestion(candidate);
+  } else {
+    items.value = [];
+    suggestionActive.value = false;
+  }
+})
+
+/**
+ * Fonction pour rechercher des suggestions
+ * @param candidate Chaîne de caractère à compléter
+ * @returns {Promise<void>}
+ */
+async function getSuggestion(candidate) {
+  isLoading.value = true;
+  try {
+    items.value = await suggestionPersonne(candidate);
+  } catch (error) {
+    request.value = candidate;
+    emit('onError', "Autcomplétion : " + error.message);
+  } finally {
+    isLoading.value = false;
+    suggestionActive.value = true;
+  }
+}
+
+/**
+ * Fonction lorsqu'on selectionne une suggestion
+ * @param value
+ */
+function selectSuggestion(value) {
+  if (value != null && typeof (value) == "object") {
+    request.value = value.suggestion
+  }
 }
 
 defineExpose({
