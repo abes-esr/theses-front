32f848bc05c8d5a4425a3ced3a0416ee7253a404
Théo Chambon <chambon@abes.fr>
2023-01-31 15:28:36 +0100

    Fix marges

--- src/components/theses/results/FacetsDrawer.vue
+++ src/components/theses/results/FacetsDrawer.vue
@@ -6,19 +6,31 @@
           STATUT
         </v-expansion-panel-title>
         <v-expansion-panel-text>
-          <v-checkbox :label="`Soutenues : ${facets.soutenue}`"></v-checkbox>
-          <v-checkbox class="pl-4" :label="`Accessibles en ligne : ${facets.accessible}`"></v-checkbox>
-          <v-checkbox :label="`En préparation : ${facets.enCours}`"></v-checkbox>
+          <v-checkbox :label="`Soutenues : ${facets.soutenue}`" value="soutenues" v-model="filtres"></v-checkbox>
+          <v-checkbox class="pl-4" :label="`Accessibles en ligne : ${facets.accessible}`" value="accessible"
+            v-model="filtres"></v-checkbox>
+          <v-checkbox :label="`En préparation : ${facets.enCours}`" value="enCours" v-model="filtres"></v-checkbox>
         </v-expansion-panel-text>
       </v-expansion-panel>
     </v-expansion-panels>
   </div>
 </template>
 <script setup>
+import { ref, watch } from 'vue'
+
 defineProps({
   facets: {
     type: Object
   }
+})
+
+const emit = defineEmits(['changeFiltres'])
+
+let filtres = ref([]);
+
+watch(filtres, async (newFiltres) => {
+  if (newFiltres.length > 0)
+    emit('changeFiltres', newFiltres);
 })
 </script>
 <style scoped lang="scss">
@@ -26,8 +38,8 @@ defineProps({
 
 .facets {
   display: flex;
-  justify-content: center;
-  align-items: center;
+  justify-content: flex-start !important;
+  align-self: center !important;
   flex-direction: column;
   padding-top: 2rem;
 
