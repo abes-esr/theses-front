2d364a481624bb42ee11a982754e00453843b00d
Théo Chambon <chambon@abes.fr>
2023-02-27 10:09:25 +0100

    Fix: layout while loading

--- src/views/ResultView.vue
+++ src/views/ResultView.vue
@@ -42,29 +42,31 @@
     </span>
 
 
-    <div v-resize="reinitializeCurrentRequest" class="result-list" v-if="dataReady">
-      <h1 class="pb-6">{{ nbResult }}{{
-        $t(currentRoute.query.domaine +
-          '.resultView.resultats')
-      }} :
-        {{ request }}</h1>
-      <div v-if="mobile" class="result-list-wrapper">
-        <ScrollToTopButton v-if="moreThanXResults(5)" class="scroll-top-wrapper" :nb-result=nbResult />
-        <GenericResultList :result="result">
-        </GenericResultList>
-        <MoreResultsButton v-if="!allResultsWereLoaded()" :loading=loading :nb-result=nbResult @changeNombre="updateNombre" />
-      </div>
-      <v-row v-else>
-        <v-col cols="11" class="colonnes-resultats">
+    <div v-resize="reinitializeCurrentRequest" class="result-list">
+      <div v-if="dataReady">
+        <h1 class="pb-6" >{{ nbResult }}{{
+          $t(currentRoute.query.domaine +
+            '.resultView.resultats')
+        }} :
+          {{ request }}</h1>
+        <div v-if="mobile" class="result-list-wrapper">
+          <ScrollToTopButton v-if="moreThanXResults(5)" class="scroll-top-wrapper" :nb-result=nbResult />
           <GenericResultList :result="result">
           </GenericResultList>
-          <MoreResultsButton :loading=loading :nb-result=nbResult @changeNombre="updateNombre" />
-        </v-col>
-        <v-col cols="1" class="colonnes-resultats">
-          <ScrollToTopButton v-if="moreThanXResults(5)" :nb-result=nbResult />
-        </v-col>
-      </v-row>
-    </div>
+          <MoreResultsButton v-if="!allResultsWereLoaded()" :loading=loading :nb-result=nbResult @changeNombre="updateNombre" />
+        </div>
+        <v-row v-else>
+          <v-col cols="11" class="colonnes-resultats">
+            <GenericResultList :result="result">
+            </GenericResultList>
+            <MoreResultsButton :loading=loading :nb-result=nbResult @changeNombre="updateNombre" />
+          </v-col>
+          <v-col cols="1" class="colonnes-resultats">
+            <ScrollToTopButton v-if="moreThanXResults(5)" :nb-result=nbResult />
+          </v-col>
+        </v-row>
+      </div>
+  </div>
   </div>
   <div class="search-filter" >
     <div class="left-side"></div>
