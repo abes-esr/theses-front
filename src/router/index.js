bdddf9e84a45d9558d212c36bdf812ad86f711bf
theo-chmbn <chambon@abes.fr>
2023-07-13 14:02:04 +0200

    scroll behavior router

--- src/router/index.js
+++ src/router/index.js
@@ -1,8 +1,6 @@
 import { createRouter, createWebHistory } from "vue-router";
 const HomeView = () => import("../views/HomeView.vue")
 const ResultView = () => import("../views/ResultView.vue")
-const TheseView = () => import("../views/TheseView.vue")
-const PersonneView = () => import("../views/PersonneView.vue")
 const DetailView = () => import("../views/DetailView.vue")
 const APropos = () => import("../components/footer-pages/APropos.vue")
 const AccessibilitePage = () => import("../components/footer-pages/AccessibilitePage.vue")
@@ -56,6 +54,9 @@ export const router = createRouter({
       component: MentionsPage
     }
   ],
+  scrollBehavior() {
+    return { top: 0, left: 0 }
+  }
 });
 
 export default router;
