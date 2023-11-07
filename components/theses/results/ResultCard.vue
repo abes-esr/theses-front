79b7b305709e4716f94bf10789a67a4b03696720
theo-chmbn <chambon@abes.fr>
2023-11-07 14:25:37 +0100

    fix bug suggestion

--- components/theses/results/ResultCard.vue
+++ components/theses/results/ResultCard.vue
@@ -1,6 +1,8 @@
 <template>
   <v-card flat class="card-container">
-    <div class="first-half" @click="$router.push({ name: 'id', params: { id: id } })">
+    <div class="first-half" @click="$router.push({
+      name: 'id', params: { id: id }, query: { domaine: route.query.domaine }
+    })">
       <v-card-title>
         <div class="card-title-wrapper line-clamp">
           <div v-if="!mobile" class="thesis-icon">
@@ -115,6 +117,7 @@ defineProps({
 
 });
 
+const route = useRoute();
 const router = useRouter();
 function linkTo(id) {
   if (id) router.push("/" + id);
