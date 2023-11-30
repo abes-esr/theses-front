f1499860cc4b2fe58885a93382e323209efd416a
theo-chmbn <chambon@abes.fr>
2023-11-30 17:11:21 +0100

    fix pagination

--- components/common/results/ResultPagination.vue
+++ components/common/results/ResultPagination.vue
@@ -76,7 +76,7 @@ const totalVisible = computed(() => {
     if (xlAndUp.value) return 8;
     else return 2;
   else
-    if (xlAndUp.value) return "";
+    if (xlAndUp.value) return "10";
     else return 2;
 })
 
