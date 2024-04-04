2fe3ce3b85bdb70993055f2d2bda0cfa4a3c9c1b
theo-chmbn <chambon@abes.fr>
2024-04-04 12:18:11 +0200

    fix url boutons export

--- components/common/results/ResultComponents.vue
+++ components/common/results/ResultComponents.vue
@@ -21,9 +21,9 @@
           <span>{{ $t("results.results") }}</span>
         </h1>
         <CommonExportQueryButton v-if="domainNameChange == 'theses'"
-          :csv-href="'/api/v1/theses/recherche/' + '?q=' + encodeURIComponent(replaceAndEscape(currentRoute.query.q)) + '&tri=' + encodeURIComponent(currentRoute.query.tri) + getFacetsRequest()"
+          :csv-href="'/api/v1/theses/rechercheCSV' + '?q=' + encodeURIComponent(replaceAndEscape(currentRoute.query.q)) + '&tri=' + encodeURIComponent(currentRoute.query.tri) + getFacetsRequest()"
           :rss-href="'/api/v1/theses/rss' + '?q=' + encodeURIComponent(replaceAndEscape(currentRoute.query.q)) + getFacetsRequest()"
-          :json-href="'/api/v1/theses/rechercheCSV' + '?q=' + encodeURIComponent(replaceAndEscape(currentRoute.query.q)) + '&nombre=10000&tri=' + encodeURIComponent(currentRoute.query.tri) + getFacetsRequest()">
+          :json-href="'/api/v1/theses/recherche' + '?q=' + encodeURIComponent(replaceAndEscape(currentRoute.query.q)) + '&nombre=10000&tri=' + encodeURIComponent(currentRoute.query.tri) + getFacetsRequest()">
         </CommonExportQueryButton>
       </div>
       <h2 class="returned-results-statement" v-else>{{ $t("results.searching") }}</h2>
