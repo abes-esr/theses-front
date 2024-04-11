2542fd6953bf4ced0229569beff4fe885c9b25bc
Clément Delafontaine <77626674+clementdelafontaine@users.noreply.github.com>
2024-04-11 15:52:16 +0200

    Update ResultComponents.vue

--- components/common/results/ResultComponents.vue
+++ components/common/results/ResultComponents.vue
@@ -60,7 +60,7 @@
 
 <script setup>
 import { useDisplay } from "vuetify";
-import { ref, watch, onMounted, computed } from "vue";
+import { ref, watch } from "vue";
 
 
 const currentRoute = useRoute();
@@ -101,14 +101,6 @@ const props = defineProps({
 
 const currentPageNumber = currentRoute.query.page ? ref(parseInt(currentRoute.query.page)) : ref(1);
 const currentShowingNumber = currentRoute.query.nb ? ref(parseInt(currentRoute.query.nb)) : ref(10);
-
-
-//Ajout du flux rss dans le head de la page, pour l'auto discovery des flux rss
-onMounted(() => {
-
-})
-
-
 
 /**
  * Fonctions
