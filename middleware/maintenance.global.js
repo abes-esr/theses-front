61d03f0dab31bb1441da6a09eec942486cec6ecf
theo-chmbn <chambon@abes.fr>
2024-04-25 12:14:14 +0200

    ajout page maintenance

--- middleware/maintenance.global.js
+++ middleware/maintenance.global.js
@@ -2,6 +2,8 @@ export default defineNuxtRouteMiddleware((to, from) => {
     const config = useRuntimeConfig();
     const isMaintenance = config.public.IS_MAINTENANCE;
 
+    console.log(isMaintenance)
+
     if (isMaintenance && to.path !== '/maintenance') {
         return navigateTo('/maintenance')
     }
