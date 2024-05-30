3b1b0ad68528fff5ba0f3b920076048a83a0ad28
Clément Delafontaine <clment.delafontaine@gmail.com>
2024-05-30 16:31:56 +0200

    FIX correction sur le routage middleware maintenance

--- app.vue
+++ app.vue
@@ -500,6 +500,10 @@ h4 {
   line-height: 1.5 !important;
   letter-spacing: 0.12em !important;
   word-spacing: 0.16em !important;
+
+  p {
+    margin-bottom: 2em !important;
+  }
 }
 
 .justification p {
--- middleware/maintenance.global.js
+++ middleware/maintenance.global.js
@@ -2,11 +2,11 @@ export default defineNuxtRouteMiddleware((to, from) => {
     const config = useRuntimeConfig();
     const isMaintenance = config.public.IS_MAINTENANCE;
 
-    if (isMaintenance && to.path !== '/maintenance') {
+    if (isMaintenance === 'true' && to.path !== '/maintenance') {
         return navigateTo('/maintenance')
     }
 
-    if (!isMaintenance && to.path === '/maintenance') {
+    if (isMaintenance !== 'true' && to.path === '/maintenance') {
         return navigateTo('/')
     }
 })
